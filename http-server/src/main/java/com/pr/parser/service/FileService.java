495bc57b9a79bf20930aa0895021cfbad728422f
Eduard Smelov <esmelov@griddynamics.com>
2024-11-07 18:19:31 +0200

    lab-2: added file uploading

--- http-server/src/main/java/com/pr/parser/service/FileService.java
+++ http-server/src/main/java/com/pr/parser/service/FileService.java
@@ -2,15 +2,17 @@ package com.pr.parser.service;
 
 import com.pr.parser.config.DocumentStorageProperties;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.util.Objects;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class FileService {
@@ -21,17 +23,20 @@ public class FileService {
             throw new NullPointerException("fileToSave is null");
         }
 
-        Path storagePath = Path.of(documentStorageProperties.getFileStoragePath());
-        Path targetFile = storagePath.resolve(fileToSave.getOriginalFilename()).normalize();
+        Path storagePath = Path.of(documentStorageProperties.getFileStoragePath()).toAbsolutePath();
 
         if (!Files.exists(storagePath)) {
             Files.createDirectories(storagePath);
+            log.info("Directory created at: {}", storagePath);
         }
+
+        Path targetFile = storagePath.resolve(Objects.requireNonNull(fileToSave.getOriginalFilename())).normalize();
 
         if (!targetFile.startsWith(storagePath)) {
             throw new SecurityException("Unsupported filename! Potential directory traversal attempt detected.");
         }
 
         Files.copy(fileToSave.getInputStream(), targetFile, StandardCopyOption.REPLACE_EXISTING);
+        log.info("File saved successfully: {}", targetFile);
     }
 }
